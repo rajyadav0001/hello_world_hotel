23f889898984768e3cede3af0a88a916c40e03f2
Rajnesh Kumar <rajneshsbl9536@gmail.com>
2024-06-27 17:03:56 +0530

    person routes file created

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const ConnectionToMongoDB = require('./db');
 const bodyParser = require('body-parser');
-const Person = require("./models/Person");
-const MenuItem = require('./models/Menu')
+const MenuItem = require('./models/Menu');
+const personRoutes = require('./routes/personRoutes');
 
 const app = express();
 const port = 5000;
@@ -14,29 +14,7 @@ app.use(bodyParser.json());
 app.get('/', (req,res)=>{
     res.send("Welcome to My Hotel !, How can i help you?");
 })
-
-//create a new person with the specified name and details
-app.post('/person', async (req,res)=>{
-     try{
-         const data = req.body;
-         const newPerson = await Person.create(data);
-         res.status(201).send(newPerson);
-     }catch(err){
-         console.log(err);
-         res.status(500).send({Error : "Internal server error"});
-     }
-})
-
-//get all the persons details
-app.get('/person', async (req,res)=>{
-    try{
-        const data = await Person.find();
-        res.status(200).send(data);
-    }catch(err){
-        console.log(err);
-        res.status(500).send({Error : "Internal server error"});
-    }
-})
+app.use('/person', personRoutes);
 
 //create a new menu item 
 app.post('/menu', async (req,res)=>{
--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -1,3 +1,44 @@
- 
+const express = require("express");
+const Person = require('../models/Person')
+const router = express.Router();
+
+//create a new person with the specified name and details
+router.post("/", async (req, res) => {
+  try {
+    const data = req.body;
+    const newPerson = await Person.create(data);
+    res.status(201).send(newPerson);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ Error: "Internal server error" });
+  }
+});
+
+//get all the persons details
+router.get("/", async (req, res) => {
+  try {
+    const data = await Person.find();
+    res.status(200).send(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ Error: "Internal server error" });
+  }
+});
+
+router.get("/:workType", async (req, res) => {
+  try {
+    const workType = req.params.workType;
+    if (workType === "chef" || workType === "waiter" || workType === "manager") {
+      const data = await Person.find({ work: workType });
+      res.status(200).send(data);
+    } else {
+      res.status(404).send({ Error: "Invalid Work Type" });
+    }
+  } catch (error) {
+    console.log(err);
+    res.status(500).send({ Error: "Internal server error" });
+  }
+});
 
 
+module.exports = router;
