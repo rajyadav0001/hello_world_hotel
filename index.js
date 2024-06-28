ddaf776e85638f02222dfd773ee94b9b6c904567
Rajnesh Kumar <rajneshsbl9536@gmail.com>
2024-06-28 23:14:01 +0530

    port changes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const personRoutes = require('./src/routes/personRoutes');
 const menuItemRoutes = require('./src/routes/menuItemRoutes')
 require('dotenv').config();
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
